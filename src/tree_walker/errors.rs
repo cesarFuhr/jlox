c799f992e5e3367c7c1d6fc81693a71bdeb48461
Cesar Cara <cesar.cara@protonmail.com>
2023-09-20 12:09:33 -0300

    parser error handling

--- src/tree_walker/errors.rs
+++ src/tree_walker/errors.rs
@@ -1,3 +1,5 @@
+use super::tokens::{Token, TokenType};
+
 pub struct Error {
     pub line: u64,
     pub message: String,
@@ -6,4 +8,22 @@ pub struct Error {
 
 pub fn report(e: Error) {
     eprintln!("[line {}] Error: {}: {}", e.line, e.place, e.message);
+}
+
+pub fn error(token: &Token, message: &String) {
+    if token.r#type == TokenType::Eof {
+        let e = Error {
+            line: token.line.to_owned(),
+            place: " at the end".to_string(),
+            message: message.to_owned(),
+        };
+        report(e)
+    }
+
+    let e = Error {
+        line: token.line.to_owned(),
+        message: message.to_owned(),
+        place: " at '".to_string() + &token.lexeme.to_owned() + "'",
+    };
+    report(e)
 }
--- src/tree_walker/parser.rs
+++ src/tree_walker/parser.rs
@@ -1,11 +1,17 @@
-use core::panicking::panic;
-
+use super::errors::error;
 use super::syntax_tree::{Binary, Expr, Grouping, Literal, Unary};
 use super::tokens::{LiteralType, Token, TokenType};
 
+#[derive(Debug)]
 struct Parser {
     tokens: Vec<Token>,
     current: usize,
+}
+
+#[derive(Debug)]
+pub struct ParseError {
+    pub token: Token,
+    pub message: String,
 }
 
 impl Parser {
@@ -13,24 +19,32 @@ impl Parser {
         Parser { tokens, current: 0 }
     }
 
-    fn expression(&mut self) -> Expr {
+    pub fn parse(&mut self) -> Option<Expr> {
+        if let Ok(expr) = self.expression() {
+            return Some(expr);
+        }
+
+        None
+    }
+
+    fn expression(&mut self) -> Result<Expr, ParseError> {
         self.equality()
     }
 
-    fn equality(&mut self) -> Expr {
-        let mut expr = self.comparison();
+    fn equality(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.comparison()?;
 
         while self.r#match(vec![TokenType::BangEqual, TokenType::EqualEqual]) {
-            let op = self.previous();
-            let right = self.comparison();
+            let op = self.previous()?;
+            let right = self.comparison()?;
             expr = Expr::Binary(Box::new(Binary::new(expr, op, right)));
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn comparison(&mut self) -> Expr {
-        let mut expr = self.term();
+    fn comparison(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.term()?;
 
         while self.r#match(vec![
             TokenType::Greater,
@@ -38,76 +52,81 @@ impl Parser {
             TokenType::Less,
             TokenType::LessEqual,
         ]) {
-            let op = self.previous();
-            let right = self.term();
+            let op = self.previous()?;
+            let right = self.term()?;
             expr = Expr::Binary(Box::new(Binary::new(expr, op, right)));
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn term(&mut self) -> Expr {
-        let mut expr = self.factor();
+    fn term(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.factor()?;
 
         while self.r#match(vec![TokenType::Minus, TokenType::Plus]) {
-            let op = self.previous();
-            let right = self.factor();
+            let op = self.previous()?;
+            let right = self.factor()?;
             expr = Expr::Binary(Box::new(Binary::new(expr, op, right)));
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn factor(&mut self) -> Expr {
-        let mut expr = self.unary();
+    fn factor(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.unary()?;
 
         while self.r#match(vec![TokenType::Slash, TokenType::Star]) {
-            let op = self.previous();
-            let right = self.unary();
+            let op = self.previous()?;
+            let right = self.unary()?;
             expr = Expr::Binary(Box::new(Binary::new(expr, op, right)));
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn unary(&mut self) -> Expr {
+    fn unary(&mut self) -> Result<Expr, ParseError> {
         if self.r#match(vec![TokenType::Bang, TokenType::Minus]) {
-            let op = self.previous();
-            let right = self.unary();
-            return Expr::Unary(Box::new(Unary::new(op, right)));
+            let op = self.previous()?;
+            let right = self.unary()?;
+            return Ok(Expr::Unary(Box::new(Unary::new(op, right))));
         }
 
         self.primary()
     }
 
-    fn primary(&mut self) -> Expr {
+    fn primary(&mut self) -> Result<Expr, ParseError> {
         if self.r#match(vec![TokenType::False]) {
-            return Expr::Literal(Literal::new(LiteralType::Bool(false)));
+            return Ok(Expr::Literal(Literal::new(LiteralType::Bool(false))));
         }
         if self.r#match(vec![TokenType::True]) {
-            return Expr::Literal(Literal::new(LiteralType::Bool(true)));
+            return Ok(Expr::Literal(Literal::new(LiteralType::Bool(true))));
         }
         if self.r#match(vec![TokenType::Nil]) {
-            return Expr::Literal(Literal::new(LiteralType::Nil));
+            return Ok(Expr::Literal(Literal::new(LiteralType::Nil)));
         }
 
         if self.r#match(vec![TokenType::Number, TokenType::String]) {
-            return Expr::Literal(Literal::new(self.previous().literal.unwrap()));
+            let prev = self.previous()?;
+            return Ok(Expr::Literal(Literal::new(prev.literal.unwrap())));
         }
 
         if self.r#match(vec![TokenType::LeftParen]) {
-            let expr = self.expression();
-            self.consume(TokenType::RightParen, "Expect ')' after expression");
-            return Expr::Grouping(Box::new(Grouping::new(expr)));
+            let expr = self.expression()?;
+            let _ = self.consume(
+                TokenType::RightParen,
+                "Expect ')' after expression".to_string(),
+            )?;
+            return Ok(Expr::Grouping(Box::new(Grouping::new(expr))));
         }
 
-        Expr::Literal(Literal::new(LiteralType::Nil))
+        let e = Parser::error(self.peek(), "expect expression".to_string());
+        Err(e)
     }
 
     fn r#match(&mut self, types: Vec<TokenType>) -> bool {
         for t in types {
             if self.check(t) {
-                self.advance();
+                let _ = self.advance();
                 return true;
             }
         }
@@ -115,12 +134,40 @@ impl Parser {
         false
     }
 
-    fn consume(&mut self, t: TokenType, message: String) -> Token {
+    fn synchronize(&mut self) -> Result<(), ParseError> {
+        let _ = self.advance();
+
+        while !self.is_at_end() {
+            let current = self.previous()?;
+            if current.r#type == TokenType::Semicolon {
+                return Ok(());
+            }
+
+            match self.peek().r#type {
+                TokenType::Class
+                | TokenType::Fun
+                | TokenType::Var
+                | TokenType::For
+                | TokenType::If
+                | TokenType::While
+                | TokenType::Print
+                | TokenType::Return => return Ok(()),
+                _ => (),
+            }
+
+            let _ = self.advance();
+        }
+
+        Ok(())
+    }
+
+    fn consume(&mut self, t: TokenType, message: String) -> Result<Token, ParseError> {
         if self.check(t) {
             return self.advance();
         }
 
-        panic(error(self.peek(), message))
+        let e = Parser::error(self.peek(), message);
+        panic!("{:?}", e);
     }
 
     fn check(&mut self, t: TokenType) -> bool {
@@ -130,7 +177,7 @@ impl Parser {
         self.peek().r#type == t
     }
 
-    fn advance(&mut self) -> Token {
+    fn advance(&mut self) -> Result<Token, ParseError> {
         if !self.is_at_end() {
             self.current += 1
         }
@@ -145,7 +192,26 @@ impl Parser {
         self.tokens.get(self.current).unwrap().to_owned()
     }
 
-    fn previous(&self) -> Token {
-        self.tokens.get(self.current - 1).unwrap().to_owned()
+    fn previous(&self) -> Result<Token, ParseError> {
+        let token = self.tokens.get(self.current - 1);
+
+        if token.is_none() {
+            return Err(ParseError {
+                token: Token {
+                    r#type: TokenType::Nil,
+                    lexeme: "".to_string(),
+                    literal: None,
+                    line: 0,
+                },
+                message: "unexpected absense of token".to_string(),
+            });
+        }
+
+        Ok(token.unwrap().to_owned())
+    }
+
+    fn error(token: Token, message: String) -> ParseError {
+        error(&token, &message);
+        ParseError { token, message }
     }
 }
