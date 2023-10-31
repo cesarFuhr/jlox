1728b7664ea588d5bf8f9c622dc8bdd6ba46ade9
Cesar Cara <cesar.cara@protonmail.com>
2023-10-31 16:32:06 -0300

    starting to eval expressions

--- src/tree_walker/syntax_tree.rs
+++ src/tree_walker/syntax_tree.rs
@@ -1,8 +1,6 @@
 use crate::tree_walker::tokens::{LiteralType, Token};
 
-pub trait PrettyPrint {
-    fn pretty_print(&self) -> String;
-}
+use super::tokens::TokenType;
 
 #[derive(PartialEq, PartialOrd, Debug)]
 pub enum Expr {
@@ -11,6 +9,14 @@ pub enum Expr {
     Unary(Box<Unary>),
     Grouping(Box<Grouping>),
     Literal(Literal),
+}
+
+pub fn ast_print(expr: Expr) -> String {
+    expr.pretty_print()
+}
+
+pub trait PrettyPrint {
+    fn pretty_print(&self) -> String;
 }
 
 impl PrettyPrint for Expr {
@@ -22,6 +28,41 @@ impl PrettyPrint for Expr {
             Unary(ref e) => e.pretty_print(),
             Grouping(ref e) => e.pretty_print(),
             Literal(ref e) => e.pretty_print(),
+        }
+    }
+}
+
+pub enum Value {
+    Nil,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+}
+
+impl Value {
+    fn is_truthy(&self) -> bool {
+        match self {
+            Value::Nil => false,
+            Value::Boolean(b) => b.to_owned(),
+            _ => true,
+        }
+    }
+
+    fn variant_eq(a: &Value, b: &Value) -> bool {
+        std::mem::discriminant(a) == std::mem::discriminant(b)
+    }
+}
+
+pub trait Eval {
+    fn eval(&self) -> Value;
+}
+
+impl Eval for Expr {
+    fn eval(&self) -> Value {
+        use Expr::*;
+        match *self {
+            Literal(ref e) => e.eval(),
+            _ => Value::Nil,
         }
     }
 }
@@ -54,6 +95,25 @@ impl PrettyPrint for Ternary {
     }
 }
 
+impl Eval for Ternary {
+    fn eval(&self) -> Value {
+        let condition = self.condition.eval();
+
+        match condition {
+            Value::Boolean(b) => {
+                if b {
+                    return self.then.eval();
+                }
+                self.r#else.eval()
+            }
+            _ => {
+                println!("we shouldn't be here... condition didn't returned a boolean");
+                Value::Nil
+            }
+        }
+    }
+}
+
 #[derive(PartialEq, PartialOrd, Debug)]
 pub struct Binary {
     left: Expr,
@@ -82,6 +142,49 @@ impl PrettyPrint for Binary {
     }
 }
 
+impl Eval for Binary {
+    fn eval(&self) -> Value {
+        let left = self.left.eval();
+        let right = self.right.eval();
+
+        if !Value::variant_eq(&left, &right) {
+            println!("types don't match in binary expression");
+            return Value::Nil;
+        }
+
+        if let (Value::Number(l), Value::Number(r)) = (&left, &right) {
+            match self.operator.r#type {
+                TokenType::Minus => return Value::Number(l - r),
+                TokenType::Slash => return Value::Number(l / r),
+                TokenType::Star => return Value::Number(l * r),
+                TokenType::Greater => return Value::Boolean(l > r),
+                TokenType::GreaterEqual => return Value::Boolean(l >= r),
+                TokenType::Less => return Value::Boolean(l < r),
+                TokenType::LessEqual => return Value::Boolean(l <= r),
+                TokenType::BangEqual => return Value::Boolean(!(l == r)),
+                TokenType::EqualEqual => return Value::Boolean(l == r),
+                _ => {
+                    print!("invalid binary expression operator");
+                    return Value::Nil;
+                }
+            }
+        }
+
+        if let (Value::String(l), Value::String(r)) = (&left, &right) {
+            match self.operator.r#type {
+                TokenType::Plus => return Value::String(l.to_owned() + r),
+                _ => {
+                    print!("invalid binary expression operator");
+                    return Value::Nil;
+                }
+            }
+        }
+
+        print!("invalid binary expression");
+        Value::Nil
+    }
+}
+
 #[derive(PartialEq, PartialOrd, Debug)]
 pub struct Unary {
     operator: Token,
@@ -103,6 +206,24 @@ impl PrettyPrint for Unary {
     }
 }
 
+impl Eval for Unary {
+    fn eval(&self) -> Value {
+        let right = self.right.eval();
+
+        match self.operator.r#type {
+            TokenType::Bang => Value::Boolean(!right.is_truthy()),
+            TokenType::Minus => match right {
+                Value::Number(n) => Value::Number(-n),
+                _ => panic!("minus should be only used in a number"),
+            },
+            _ => {
+                print!("we shouldn't be here...");
+                Value::Nil
+            }
+        }
+    }
+}
+
 #[derive(PartialEq, PartialOrd, Debug)]
 pub struct Grouping {
     expression: Expr,
@@ -117,6 +238,12 @@ impl Grouping {
 impl PrettyPrint for Grouping {
     fn pretty_print(&self) -> String {
         format!("(group {})", self.expression.pretty_print())
+    }
+}
+
+impl Eval for Grouping {
+    fn eval(&self) -> Value {
+        self.expression.eval()
     }
 }
 
@@ -140,6 +267,13 @@ impl PrettyPrint for Literal {
     }
 }
 
-pub fn ast_print(expr: Expr) -> String {
-    expr.pretty_print()
+impl Eval for Literal {
+    fn eval(&self) -> Value {
+        match self.value.to_owned().unwrap_or(LiteralType::Nil) {
+            LiteralType::Number(v) => Value::Number(v),
+            LiteralType::String(v) => Value::String(v),
+            LiteralType::Bool(v) => Value::Boolean(v),
+            LiteralType::Nil => Value::Nil,
+        }
+    }
 }
